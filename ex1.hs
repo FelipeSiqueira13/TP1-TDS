0d68877ad0238b2a25eff402c6a6798454a26469
Felipe Espinheira <fsiqesp2@gmail.com>
2026-03-17 17:02:30 +0000

    geradores criados

--- ex1.hs
+++ ex1.hs
@@ -1,3 +1,11 @@
+import Data.List
+import Test.HUnit
+import Test.QuickCheck
+import Test.QuickCheck.Function
+import Test.QuickCheck.Gen (genFloat)
+import Foreign (free)
+import Data.Ratio 
+
 data Movimento = Credito Float
                 | Debito Float
                 deriving Show
@@ -64,3 +72,55 @@ banc´aria).
 Tenha ainda em conta que a lista de extratos Extratos tem no¸c˜ao de ordem, isto ´e, o valor
 inicial do extrato seguinte deve corresponder ao resultado da fun¸c˜ao saldo do extrato anterior.
 -}
+
+
+genMovimento :: Gen Movimento
+genMovimento = do
+                tipo <- frequency[(50, return Debito),(50, return Credito)]
+                valor <- choose(1,50000)
+                return $ tipo (fromRational (valor % 100))
+
+genData :: Gen Data
+genData = do
+        dia <- elements[1..31]
+        mes <- elements[1..12]
+        ano <- elements[2000..2026]
+        return $ D dia mes ano
+
+genDinheiro :: Gen Float
+genDinheiro = do
+    v <- choose (1, 50000) :: Gen Integer
+    return (fromRational (v % 100))
+
+
+genOperacoes :: Int -> Gen [(Data, String, Movimento)]
+genOperacoes s = do
+    datah <- genData
+    str   <- vectorOf 5 (elements ['A'..'Z'])
+    mov   <- genMovimento
+    rest  <- frequency[(1, return []),(s, genOperacoes (s - 1))]
+    return ((datah, str, mov) : rest)
+
+instance Arbitrary Extracto where
+    arbitrary = sized genExtracto
+genExtracto :: Int -> Gen Extracto
+genExtracto s = do
+            dinIni <- genDinheiro
+            lista <- genOperacoes s
+            return (Ext dinIni lista)
+            
+instance Arbitrary Extractos where
+    arbitrary = sized genExtractos
+
+genExtractos :: Int -> Gen Extractos
+genExtractos s = do
+    xs <- genExtractos' (s-1)
+    return (Extractos xs)
+
+genExtractos' :: Int -> Gen [Extracto]
+genExtractos' s = do
+    atual <- genExtracto 3
+    resto <- frequency [(1, return []), (s, genExtractos' (s - 1))]
+    return (atual : resto)
+            
+            
